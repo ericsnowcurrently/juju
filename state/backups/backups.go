d35ed33c800bb9fd3fbff6ca9ee3b97c278c0fd2
Eric Snow <ericsnowcurrently@gmail.com>
2014-08-07 22:31:24 -0600

    Add Create() to the backups abstraction.

--- state/backups/backups.go
+++ state/backups/backups.go
@@ -6,6 +6,10 @@
 package backups
 
 import (
+	"io"
+	"time"
+
+	"github.com/juju/errors"
 	"github.com/juju/loggo"
 	"github.com/juju/utils/filestorage"
 
@@ -16,6 +20,11 @@ var logger = loggo.GetLogger("juju.state.backup")
 
 // Backups is an abstraction around all juju backup-related functionality.
 type Backups interface {
+	// Create creates and stores a new juju backup archive and returns
+	// its associated metadata.
+	Create(origin *Origin, notes string) (*Metadata, error)
+	// Get returns the metadata and archive file associated with the ID.
+	Get(id string) (*Metadata, io.ReadCloser, error)
 }
 
 type backups struct {
@@ -33,4 +42,43 @@ func NewBackups(
 		storage: stor,
 	}
 	return &b
+}
+
+// Create creates and stores a new juju backup archive and returns
+// its associated metadata.
+func (b *backups) Create(origin *Origin, notes string) (*Metadata, error) {
+	// Prep the metadata.
+	meta := NewMetadata("", 0, *origin, notes)
+
+	// Create the archive.
+	archive, err := create(b.config, meta)
+	if err != nil {
+		return nil, errors.Annotate(err, "error creating backup archive")
+	}
+	defer archive.Close()
+
+	// Store the archive.
+	finished := time.Now().UTC()
+	meta.finished = &finished
+	_, err = b.storage.Add(meta, archive)
+	if err != nil {
+		return nil, errors.Annotate(err, "error storing backup archive")
+	}
+
+	return meta, nil
+}
+
+// Get returns the metadata and archive file associated with the ID.
+func (b *backups) Get(id string) (*Metadata, io.ReadCloser, error) {
+	rawmeta, archive, err := b.storage.Get(id)
+	if err != nil {
+		return nil, nil, errors.Trace(err)
+	}
+
+	meta, ok := rawmeta.(*Metadata)
+	if !ok {
+		panic("did not get a backup.Metadata value from storage")
+	}
+
+	return meta, archive, nil
 }
