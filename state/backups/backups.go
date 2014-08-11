028d9e91c8e16e8736b71dfafbb14e2946b6cbb6
Eric Snow <ericsnowcurrently@gmail.com>
2014-08-11 17:53:20 -0600

    Add a backups abstraction.

--- state/backups/backups.go
+++ state/backups/backups.go
@@ -7,6 +7,30 @@ package backups
 
 import (
 	"github.com/juju/loggo"
+	"github.com/juju/utils/filestorage"
+
+	"github.com/juju/juju/state/backups/config"
 )
 
-var logger = loggo.GetLogger("juju.state.backups")
+var logger = loggo.GetLogger("juju.state.backup")
+
+// Backups is an abstraction around all juju backup-related functionality.
+type Backups interface {
+}
+
+type backups struct {
+	config  config.BackupsConfig
+	storage filestorage.FileStorage
+}
+
+// NewBackups returns a new Backups value using the provided DB info and
+// file storage.
+func NewBackups(
+	config config.BackupsConfig, stor filestorage.FileStorage,
+) Backups {
+	b := backups{
+		config:  config,
+		storage: stor,
+	}
+	return &b
+}
