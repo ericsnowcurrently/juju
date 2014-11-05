4c745b1af9a3d05b5a1f7e97352edde5f366cfd0
Eric Snow <ericsnowcurrently@gmail.com>
2014-11-05 09:16:19 -0700

    Add a package doc comment.

---
+++ state/backups/archive/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// archive provides support for working with backup archives of juju's
+// state.
+package archive
