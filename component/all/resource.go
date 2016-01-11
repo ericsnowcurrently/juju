97198ad8d9b300ed8c9a5dfce4fbcbca5a13ce3a
Eric Snow <ericsnowcurrently@gmail.com>
2016-01-11 16:24:49 -0700

    Post-merge fixes.

--- component/all/resource.go
+++ component/all/resource.go
@@ -5,7 +5,6 @@ package all
 
 import (
 	"bytes"
-	"io"
 	"os"
 
 	"github.com/juju/errors"
@@ -148,7 +147,7 @@ func (r resources) registerPublicCommands() {
 	commands.RegisterEnvCommand(func() envcmd.EnvironCommand {
 		return cmd.NewShowServiceCommand(cmd.ShowServiceDeps{
 			NewClient: func(c *cmd.ShowServiceCommand) (cmd.ShowServiceClient, error) {
-				return r.newClient(c)
+				return r.newClient(c.NewAPIRoot)
 			},
 		})
 	})
