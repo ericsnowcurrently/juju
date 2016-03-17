18d8e319b49d3afe10378a3117e1bba676815ebb
Eric Snow <ericsnowcurrently@gmail.com>
2016-03-16 19:41:25 -0600

    Revert "Merge pull request #4673 from davecheney/apiserver-remove-resource-api-dependency"
    This reverts commit 61607abf16aec765b2f6a106a3f43bb78691e2c3, reversing
    changes made to 59b80a4bb527c1ade127d7baa8fe4fb4895be617.
    
    Conflicts:
    	apiserver/apiserver.go

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -24,6 +24,7 @@ import (
 	"github.com/juju/juju/apiserver/common"
 	"github.com/juju/juju/apiserver/common/apihttp"
 	"github.com/juju/juju/apiserver/params"
+	resourceapi "github.com/juju/juju/resource/api"
 	"github.com/juju/juju/rpc"
 	"github.com/juju/juju/rpc/jsoncodec"
 	"github.com/juju/juju/state"
@@ -384,7 +385,7 @@ func (srv *Server) endpoints() []apihttp.Endpoint {
 	logSinkHandler := srv.trackRequests(newLogSinkHandler(httpCtxt, srv.logDir))
 	debugLogHandler := srv.trackRequests(newDebugLogDBHandler(httpCtxt))
 
-	add("/model/:modeluuid/services/:service/resources/:resource",
+	add(mux, "/model/:modeluuid"+resourceapi.HTTPEndpointPattern,
 		newResourceHandler(httpCtxt),
 	)
 	add("/model/:modeluuid/units/:unit/resources/:resource",
